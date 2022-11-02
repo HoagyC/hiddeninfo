48f99a915a9e555d646d63892c61201933bcfeb5
HoagyC <hoagycunningham@gmail.com>
2022-11-02 17:42:54 +0000

    Get CLIP working without memory leak.

--- CUB/CLIP_CUB_process.py
+++ CUB/CLIP_CUB_process.py
@@ -1,34 +1,42 @@
 import os 
 import pickle
 from pathlib import Path
+import torch
+import gc
 
 from PIL import Image
 from tqdm import tqdm
+
 
 from transformers import CLIPProcessor, CLIPModel
 
 # To be run from the CUB folder
 IMAGE_PATH = Path("../CUB_200_2011/images")
-OUTPUT_PATH = Path("../CUB_proccesed/CLIP_embeddings.pkl")
+OUTPUT_PATH = Path("../CUB_processed/CLIP_embeddings.pkl")
 MODEL_NAME = "openai/clip-vit-base-patch32"
-
-processor = CLIPProcessor.from_pretrained(MODEL_NAME)
-model = CLIPModel.from_pretrained(MODEL_NAME)
 
 folders = sorted(os.listdir(IMAGE_PATH))
 
+processor = CLIPProcessor.from_pretrained(MODEL_NAME)
+model = CLIPModel.from_pretrained(MODEL_NAME)
+model.to("cuda")
+
 embeddings = {}
 
-for bird_folder in tqdm(folders):
-    bird_folder_path = IMAGE_PATH / bird_folder
+for ndx in tqdm(range(len(folders))):
+    with torch.no_grad():
+        bird_folder = folders[ndx]
+        bird_folder_path = IMAGE_PATH / bird_folder
 
-    for image_name in os.listdir(bird_folder_path):
-        image_path = bird_folder_path / image_name 
-        image = Image.open(image_path)
-        processed_image = processor(images=image, return_tensors="pt")
-        output = model.get_image_features(**processed_image)
+        for image_name in os.listdir(bird_folder_path):
+            image_path = bird_folder_path / image_name 
+            image = Image.open(image_path)
+            processed_image = processor(images=image, return_tensors="pt").to("cuda")
+            output = model.get_image_features(**processed_image)
 
-        embeddings[image_path] = output
+            embeddings[image_path] = output
+
+
 
 with open(OUTPUT_PATH, "wb") as f:
     pickle.dump(embeddings, f)
