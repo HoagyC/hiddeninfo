fe863cc6978af2e9fc59b9c5a9c0e24798f81ccf
hoagyc <hoagycunningham@gmail.com>
2022-11-07 17:37:46 +0000

    Add Experiment field to TrainResult.

--- experiments.py
+++ experiments.py
@@ -1,51 +1,7 @@
-from typing import List
-from typing import Optional
+from typing import List, Optional
 import dataclasses
 
-
-@dataclasses.dataclass
-class Experiment:
-    tag: str
-
-    use_class: bool = False
-
-    # Model setup
-    vector_size: int = 20
-    latent_size: int = 25
-    preferred_rep_size: int = 10
-    n_hidden_layers: int = 0
-    hidden_size: int = 30
-    batch_size: int = 32
-    vector_p2_scale: int = 1
-    representation_loss: Optional[float] = 5
-    reconstruction_loss_scale: float = 1
-    dropout_prob: Optional[float] = None
-    l1_loss: Optional[float] = None
-    l2_loss: Optional[float] = None
-    # TODO: Use enums for this & other strings?
-    activation_fn: str = "relu"
-
-    # Training setup
-    num_batches: int = 10_000
-    seed: Optional[int] = None
-    has_missing_knowledge: bool = False
-    shuffle_decoders: bool = False
-    n_models: int = 1
-    load_encoders_from_tag: Optional[str] = None
-    load_decoders_from_tag: Optional[str] = None
-    give_full_info: bool = False
-    learning_rate: float = 1e-03
-
-    # Representation loss options
-    loss_geometry: str = "simple"
-    loss_quadrants: str = "all"
-    # Useful range is [1, 1023] for 'bin_val' and [1, 9] for 'bin_sum'
-    quadrant_threshold: int = 0
-    sparsity: int = 1  # repr_loss scaled up by sparsity, applied every 1/sparsity
-    latent_noise_std: float = 0
-    latent_masking: bool = False
-    latent_masking_incentive: float = 0.1
-
+from classes import Experiment
 
 baseline = Experiment(
     tag="baseline",
--- sparsity.py
+++ sparsity.py
@@ -1,7 +1,10 @@
 import dataclasses
+from matplotlib import pyplot as plt
+import numpy as np
 from pathlib import Path
 import pickle
 import streamlit as st
+from typing import Dict
 
 from classes import TrainResult, StepResult, Model, Loss
 from main import _run_experiments
@@ -23,36 +26,128 @@ base = Experiment(
     representation_loss=1,
 )    
 
-results = []
-out_loc = Path("out/multi_seq_results.pkl")
-for quadrant_sparsity in range(4, 6):
-    with open(out_loc, "rb") as f:
-        results = pickle.load(f)
-    st.header(f"running sparsity {quadrant_sparsity}")
-    seq_sparse_decoder = dataclasses.replace(
-        base,
-        tag="seq_sparse_dec" + str(quadrant_sparsity),
-        loss_quadrants="bin_sum",
-        quadrant_threshold=quadrant_sparsity,
-        give_full_info=True,
-        num_batches=10000,
-    )
 
-    seq_sparse_encoder = dataclasses.replace(
-        base,
-        tag="seq_sparse_enc" + str(quadrant_sparsity),
-        loss_quadrants="bin_sum",
-        quadrant_threshold=quadrant_sparsity,
-        reconstruction_loss_scale=0,
-        num_batches=10000,
+def _get_output_vals(train_result: TrainResult) -> Dict:
+    last_step = max(step_result.step for step_result in train_result.step_results)
+    reconstruction_loss_p2 = np.mean(
+        [
+            step_result.reconstruction_loss_p2
+            for step_result in train_result.step_results
+            if step_result.step >= last_step * 0.9
+        ]
     )
-    seq_sparse_test = dataclasses.replace(
-        base,
-        tag="sequential_test" + str(quadrant_sparsity),
-        load_decoders_from_tag=seq_sparse_decoder.tag,
-        load_encoders_from_tag=seq_sparse_encoder.tag,
-        num_batches=2000,
+    reconstruction_loss_p1 = np.mean(
+        [
+            step_result.reconstruction_loss_p1
+            for step_result in train_result.step_results
+            if step_result.step >= last_step * 0.9
+        ]
     )
-    results += _run_experiments(seq_sparse_decoder, seq_sparse_encoder, seq_sparse_test)
-    with open(out_loc, "wb") as f:
-        pickle.dump(results, f)
+    output = dict(
+        tag=train_result.tag, 
+        reconstruction_loss_p1=reconstruction_loss_p1,
+        reconstruction_loss_p2=reconstruction_loss_p2,
+        )
+    
+    return output
+
+
+
+def run_independent_sparse() -> None:
+    results = []
+    out_loc = Path("out/multi_seq_results.pkl")
+    for quadrant_sparsity in range(1, 6):
+        with open(out_loc, "rb") as f:
+            results = pickle.load(f)
+        st.header(f"running sparsity {quadrant_sparsity}")
+        ind_sparse_decoder = dataclasses.replace(
+            base,
+            tag="seq_sparse_dec" + str(quadrant_sparsity),
+            loss_quadrants="bin_sum",
+            quadrant_threshold=quadrant_sparsity,
+            give_full_info=True,
+            num_batches=10000,
+        )
+
+        ind_sparse_encoder = dataclasses.replace(
+            base,
+            tag="seq_sparse_enc" + str(quadrant_sparsity),
+            loss_quadrants="bin_sum",
+            quadrant_threshold=quadrant_sparsity,
+            reconstruction_loss_scale=0,
+            num_batches=10000,
+        )
+        ind_sparse_test = dataclasses.replace(
+            base,
+            tag="sequential_test" + str(quadrant_sparsity),
+            load_decoders_from_tag=seq_sparse_decoder.tag,
+            load_encoders_from_tag=seq_sparse_encoder.tag,
+            num_batches=2000,
+        )
+        results += _run_experiments(seq_sparse_decoder, seq_sparse_encoder, seq_sparse_test)
+        with open(out_loc, "wb") as f:
+            pickle.dump(results, f)
+        
+def run_distill_sparse() -> None:
+    results = []
+    out_loc = Path("out/multi_distill_results.pkl")
+    for quadrant_sparsity in range(1, 6):
+        sparse_general = dataclasses.replace(
+            base,
+            tag="sparse_general",
+            loss_quadrants="bin_sum",
+            quadrant_threshold=4,
+            num_batches=30_000,
+            sparsity=10,
+        )
+        sparse_general_enc = dataclasses.replace(
+            sparse_general,
+            tag="sparse_general_enc",
+            load_decoders_from_tag=sparse_general.tag,
+            shuffle_decoders=True,
+        )
+        sparse_general_dec = dataclasses.replace(
+            sparse_general,
+            tag="sparse_general_dec",
+            load_encoders_from_tag=sparse_general.tag,
+            shuffle_decoders=True,
+        )
+
+
+
+
+if __name__ == "__main__":
+    output_dir = Path("out")
+    with open(output_dir / "multi_seq_results.pkl", "rb") as f:
+        independent_results = pickle.load(f)
+    independent_vals = [_get_output_vals(tr) for tr in independent_results if "test" in tr.tag]
+    
+    with open(output_dir / "multiresult.pkl", "rb") as f:
+        distill_results = pickle.load(f)
+    distill_enc_vals = [_get_output_vals(tr) for tr in distill_results if "enc" in tr.tag]
+    distill_dec_vals = [_get_output_vals(tr) for tr in distill_results if "dec" in tr.tag]
+        
+    fig, (ax1, ax2) = plt.subplots(1, 2)
+    sparsity_thresholds = list(range(1, 6))
+    
+    result_tags = ["independent", "retrain_encs", "retrain_decs"]
+    result_lists = [independent_vals, distill_enc_vals, distill_dec_vals]
+    
+    for name, result_set in zip(result_tags, result_lists):
+        p1s = [v["reconstruction_loss_p1"] for v in result_set]
+        p2s = [v["reconstruction_loss_p2"] for v in result_set]
+        print(p1s)
+        ax1.plot(sparsity_thresholds, p1s, label=name)
+        ax2.plot(sparsity_thresholds, p2s, label=name)
+    
+    ax1.legend()
+    ax1.set_ylim(bottom=0)
+    ax1.set_xlabel("Sparsity threshold")
+    ax1.set_ylabel("p1 reconstruction loss")
+
+    ax2.set_ylim(bottom=0)
+    ax2.legend()
+    ax2.set_xlabel("Sparsity threshold")
+    ax2.set_ylabel("p2 reconstruction loss")
+    
+    plt.show()
